cbdec57c566f3266d21b2a25583c0027b6660e00
Lukey <ldipaola66@gmail.com>
2020-09-26 23:06:28 +1000

    create user schema

---
+++ models/user.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    } 
+    }, {timestamps: true});
+  
+  const User = mongoose.model("User", UserSchema);
+  
+  export default User;
