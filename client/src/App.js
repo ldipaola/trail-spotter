8bcfa677b085fa97658e856da52db3568a9cd929
Lukey <ldipaola66@gmail.com>
2020-09-26 23:53:30 +1000

    added nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-//import './App.css'
+import './App.css'
 import store from './store'
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/nav/Navbar"
 import Login from "./components/pages/Login";
 import Home from "./components/pages/Home";
 import Blog from "./components/pages/Blog";
@@ -14,9 +14,12 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div>
+            <Navbar />
+            <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/blog" component={Blog} />
+            <Route path="/login" component={Login} />
+            <Route path="/blog" component={Blog} />
+            </Switch>
           </div>
         </Router>
       </Provider>
---
+++ client/src/components/nav/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      <section className="navbar-section">
+        <Link to="/" className="btn btn-link">
+          Home
+        </Link>
+        <Link to="/blog" className="btn btn-link">
+          Blog
+        </Link>
+        <Link to="/login" className="btn btn-link">
+          Login
+        </Link>
+      </section>
+      <section className="navbar-center"></section>
+      <section className="navbar-section">
+        <Link to="#" className="btn btn-link">
+          Sign Up
+        </Link>
+        <Link to="/login" className="btn btn-link">
+          Login
+        </Link>
+      </section>
+    </header>
+  );
+}
