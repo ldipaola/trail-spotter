73ece8d8bf34b803cbd9cad504f3434e61e3affa
Lukey <ldipaola66@gmail.com>
2020-09-28 23:56:17 +1000

    login post request

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,49 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login(props) {
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(loginDetails)
+
+    
+    fetch("/api/login", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginDetails),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    
+
+  }
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    if (event.target.id === "login-email") {
+      setLoginDetails({
+        ...loginDetails,
+        email: value,
+      })
+    }
+    else if (event.target.id === "login-password"){
+      setLoginDetails({
+        ...loginDetails,
+        password: value,
+      })
+    }
+  }
+
   return (
     <div className="container">
       <div className="columns">
         <div className="column col-3 col-mx-auto">
-          <div class="card">
-            <div class="card-header">
-              <div class="card-title h5">Login to begin your adventure!</div>
+          <div className="card">
+            <div className="card-header">
+              <div className="card-title h5">Login to begin your adventure!</div>
             </div>
-            <div class="card-body">
-              <form class="form-horizontal">
-                <div class="form-group">
-                  <label class="form-label" for="login-email">
+            <div className="card-body">
+              <form className="form-horizontal" onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label className="form-label" for="login-email">
                     Email
                   </label>
                   <input
-                    class="form-input"
+                    className="form-input"
                     type="text"
                     id="login-email"
                     placeholder="Email"
+                    onChange={handleChange}
                   />
                   <br />
-                  <label class="form-label" for="login-password">
+                  <label className="form-label" for="login-password">
                     Password
                   </label>
                   <input
-                    class="form-input"
+                    className="form-input"
                     type="password"
                     id="login-password"
                     placeholder="Password"
+                    onChange={handleChange}
                   />
                 </div>
 
-                <div class="form-group">
-                  <label class="form-checkbox">
-                    <input type="checkbox" />
-                    <i class="form-icon"></i> Remember me
-                  </label>
-                </div>
                 <button className="btn">Login</button>
               </form>
             </div>
-            <div class="card-footer">
-            Don't have an account? Sign up <a href="#">here</a> 
+            <div className="card-footer">
+            Don't have an account? Sign up <a href="/signup">here</a> 
             </div>
           </div>
         </div>
