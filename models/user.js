b58a27988c7651760dde2d1f325f84a558d029ea
Lukey <ldipaola66@gmail.com>
2020-09-28 23:53:22 +1000

    add bcrypt password hashing

--- models/user.js
+++ models/user.js
@@ -1,16 +1,26 @@
-import mongoose from 'mongoose';
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     email: {
         type: String,
+        required: true
     },
     password: {
         type: String,
+        required: true
     } 
     }, {timestamps: true});
+
+    UserSchema.pre("save", async function(next) {
+        const user = this;
+
+        if (user.isModified("password")) {
+            user.password = await bcrypt.hash(user.password, 8);
+        }
+        next();
+    });
   
-  const User = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
   
-  export default User;
