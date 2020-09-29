0d1f515f5087b13765302f71c76c6f9880414232
Lukey <ldipaola66@gmail.com>
2020-09-30 00:10:30 +1000

    add email validator and bcrypt

--- models/user.js
+++ models/user.js
@@ -1,26 +1,40 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
+const { isEmail } = require('validator');
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      unique: true,
+      validate: [isEmail, 'Please enter a valid email']
     },
     password: {
-        type: String,
-        required: true
-    } 
-    }, {timestamps: true});
+      type: String,
+      required:[true, 'Please enter a password'],
+    },
+  },
+  { timestamps: true }
+);
 
-    UserSchema.pre("save", async function(next) {
-        const user = this;
+UserSchema.pre("save", function (next) {
+  if (this.isNew) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(this.password, salt);
+    this.password = hash;
+  }
+  next();
+});
 
-        if (user.isModified("password")) {
-            user.password = await bcrypt.hash(user.password, 8);
-        }
-        next();
-    });
-  
+UserSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.methods.getJWT = function () {
+  return JWT.sign({ userId: this._id }, jwtSecret);
+};
+
 module.exports = mongoose.model("User", UserSchema);
-  
