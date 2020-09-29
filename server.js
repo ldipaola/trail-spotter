d39d709fd9dae463e558fe14128a46f57b8a6afa
Lukey <ldipaola66@gmail.com>
2020-09-29 23:20:19 +1000

    add morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const passport = require("./config/passport");
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use(passport.initialize());
 app.use(passport.session());
