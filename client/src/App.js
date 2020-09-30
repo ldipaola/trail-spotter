4da6338407e1dc67d67ec692bb6d0d46b167d384
Lukey <ldipaola66@gmail.com>
2020-09-30 23:31:13 +1000

    add route paths

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import store from './store'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/nav/Navbar"
 import Login from "./components/pages/Login";
+import Signup from "./components/pages/Signup"
 import Home from "./components/pages/Home";
 import Blog from "./components/pages/Blog";
+import User from "./components/pages/User";
 
 class App extends Component {
   render() {
@@ -18,7 +20,9 @@ class App extends Component {
             <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
             <Route path="/blog" component={Blog} />
+            <Route path="/user" component={User} />
             </Switch>
           </div>
         </Router>
