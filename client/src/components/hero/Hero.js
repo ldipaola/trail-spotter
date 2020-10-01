ecb97632b089cfc98fb34d507b6648135c205780
Lukey <ldipaola66@gmail.com>
2020-10-01 19:56:25 +1000

    add hero

---
+++ client/src/components/hero/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function Hero() {
+  return (
+    <div className="hero hero-image">
+      <div className="hero-text">
+        <h1>Mountain Biking</h1>
+        <p>Mountain Biking community</p>
+      </div>
+    </div>
+  );
+}
