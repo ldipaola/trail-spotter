0fc05e5b01a61599e594172bf50abec37d94bb41
Lukey <ldipaola66@gmail.com>
2020-10-01 19:59:03 +1000

    nav css

--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 
 export default function Navbar() {
   return (
-    <header className="navbar">
-      <section className="navbar-section">
-        <Link to="/" className="btn btn-link">
+    <header className="navbar p-2">
+      <section className="navbar-section text-bold">
+        <Link to="/" className="btn btn-link nav-link">
           Home
         </Link>
-        <Link to="/blog" className="btn btn-link">
+        <Link to="/map" className="btn btn-link nav-link">
+          Map
+        </Link>
+        <Link to="/blog" className="btn btn-link nav-link">
           Blog
         </Link>
-        <Link to="/login" className="btn btn-link">
-          Login
+        <Link to="/about" className="btn btn-link nav-link">
+          About
         </Link>
       </section>
       <section className="navbar-center"></section>
       <section className="navbar-section">
-        <Link to="/signup" className="btn btn-link">
+        <Link to="/signup" className="btn btn-primary" style={{marginRight: "8px"}}>
           Sign Up
         </Link>
-        <Link to="/login" className="btn btn-link">
+        <Link to="/login" className="btn">
           Login
         </Link>
       </section>
