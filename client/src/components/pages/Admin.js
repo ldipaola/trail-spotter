cbc2e68c7cd04fca54fd8f04045f2bad020d5e63
Lukey <ldipaola66@gmail.com>
2020-10-02 23:48:21 +1000

    admin page

---
+++ client/src/components/pages/Admin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostForm from "../post-form/PostForm"
+
+export default function Admin() {
+    return (
+        <div className="container grid-lg">
+            <PostForm />
+        </div>
+    )
+}
