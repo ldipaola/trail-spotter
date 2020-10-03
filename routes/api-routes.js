eb11320832e26684e8003eee67cfddb13e645b3b
Lukey <ldipaola66@gmail.com>
2020-10-03 22:41:28 +1000

    added auth user route and refactored

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,69 +1,71 @@
 // Requiring our models and passport as we've configured it
 const router = require("express").Router();
-const db = require('../models');
-const ObjectId = require('mongodb').ObjectId;
+const db = require("../models");
+const ObjectId = require("mongodb").ObjectId;
 const passport = require("../config/passport");
 
-
 router.get("/api/blog", (req, res) => {
-    // Will add to this later
-    res.json({
-      test:"test blog"})
+  // Will add to this later
+  res.json({
+    test: "test blog",
+  });
 });
 
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  router.post("/api/login", passport.authenticate("local"), (req, res) => {
-    res.json({
-      email: req.user.email,
-      id: req.user.id
-    });
+// Using the passport.authenticate middleware with our local strategy.
+// If the user has valid login credentials, send them to the members page.
+// Otherwise the user will be sent an error
+router.post("/api/login", passport.authenticate("local"), (req, res) => {
+  res.json({
+    email: req.user.email,
+    id: req.user.id,
   });
+});
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely.
-  // otherwise send back an error
-  router.post("/api/signup", async (req, res) => {
-      const user = new db.User(req.body);
-      try {
+// Route for signing up a user. The user's password is automatically hashed and stored securely.
+// otherwise send back an error
+router.post("/api/signup", async (req, res) => {
+  db.User.findOne({ email: req.body.email }),
+    async (err, doc) => {
+      if (err) throw err;
+      if (doc) res.send("User Already Exists");
+      if (!doc) {
+        const user = new db.User(req.body);
         await user.save();
-        res.json("Success");
-      } catch (e) {
-        res.status(401).json(err);
+        res.send("Success");
       }
-  });
+    };
+});
 
-  // Route for logging user out
-  router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
+// Route for logging user out
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send("Success");
+});
 
-  // Route for getting some data about our user to be used client side
-  router.get("/api/user_data", (req, res) => {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      res.json({
-        email: req.user.email,
-        id: req.user._id,
-        createdAt: req.user.createdAt,
-      });
-    }
-  });
+// Route for getting some data about our user to be used client side
+router.get("/api/userAuthenticated", (req, res) => {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    res.json({
+      id: req.user._id,
+      email: req.user.email,
+    });
+  }
+});
 
-  router.post("/api/post", async (req, res) => {
-    const {title, author, body} = req.body;
-    await db.Blog.create(
-    {
-      title:  title,
-      author: author,
-      body:   body,
-    }
-    ).then((blogPost) => {
+router.post("/api/post", async (req, res) => {
+  const { title, author, body } = req.body;
+  await db.Blog.create({
+    title: title,
+    author: author,
+    body: body,
+  })
+    .then((blogPost) => {
       res.json(blogPost);
-    }).catch((err) => console.log(err)) 
-  });
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
