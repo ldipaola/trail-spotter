9a18a89aa7aeea50729a071a30da06d0c586c217
Lukey <ldipaola66@gmail.com>
2020-10-03 22:44:47 +1000

    user context and conditional rendering

--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -1,8 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./style.css";
+import UserContext from "../../utils/UserContext";
+import { logout } from "../../utils/logout";
 
 export default function Navbar() {
+  let history = useHistory();
+
+
+  const { user, setUser } = useContext(UserContext);
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    const logOut = await logout();
+    setUser(null);
+    history.push("/");
+  };
+
   return (
     <header className="navbar p-2">
       <section className="navbar-section text-bold">
@@ -20,14 +34,28 @@ export default function Navbar() {
         </Link>
       </section>
       <section className="navbar-center"></section>
-      <section className="navbar-section">
-        <Link to="/signup" className="btn btn-primary" style={{marginRight: "8px"}}>
-          Sign Up
-        </Link>
-        <Link to="/login" className="btn">
-          Login
-        </Link>
-      </section>
+      {!user ? (
+        <section className="navbar-section">
+          {" "}
+          <Link
+            to="/signup"
+            className="btn btn-primary"
+            style={{ marginRight: "8px" }}
+          >
+            Sign Up
+          </Link>
+          <Link to="/login" className="btn">
+            Login
+          </Link>{" "}
+        </section>
+      ) : (
+        <section className="navbar-section">
+          {" "}
+          <Link to="/logout" className="btn" onClick={handleLogout}>
+            Logout
+          </Link>
+        </section>
+      )}
     </header>
   );
 }
