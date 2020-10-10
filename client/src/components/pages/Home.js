59672be2e9030a334911fc2e85a1516ef3b0a671
Lukey <ldipaola66@gmail.com>
2020-10-10 21:49:15 +1100

    container

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home(props) {
   const { user } = useContext(UserContext);
 
   return (
-    <div>
+    <div className="container grid-lg">
       <h1>Welcome</h1>
 
       {user ? <pre>{user.email}</pre> : <pre>Not Logged in</pre>}
