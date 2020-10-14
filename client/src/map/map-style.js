62ff0eb6e6db85911a8c6645eeee555130d7f29c
Lukey <ldipaola66@gmail.com>
2020-10-14 23:19:48 +1100

    map styles

---
+++ client/src/map/map-style.js
@@ -0,0 +1,22 @@
+export const dataLayer = {
+    id: 'data',
+    type: 'line',
+    paint: {
+      'line-color': {
+        property: 'value',
+        stops: [
+          [0, '#F2F12D'],
+          [1, '#EED322'],
+          [2, '#E6B71E'],
+          [3, '#DA9C20'],
+          [4, '#CA8323'],
+          [5, '#B86B25'],
+          [6, '#A25626'],
+          [7, '#8B4225'],
+          [8, '#723122']
+        ]
+  
+      },
+      'line-opacity': 0.8,
+    }
+  };
