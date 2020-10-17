10448fcbc13f395bef070e6e3833b869871ceee7
Lukey <ldipaola66@gmail.com>
2020-10-18 00:29:54 +1100

    change to google map

--- client/src/components/pages/Map.js
+++ client/src/components/pages/Map.js
@@ -1,30 +1,43 @@
-import React, {useState} from "react";
-import ReactMapGL, {Marker, Source, Layer} from "react-map-gl";
-import mapData from "../../map/data/pre_recweb_track.geojson";
-import { dataLayer } from "../../map/map-style";
+import React, {useState, useRef, useCallback} from "react";
+import { GoogleMap, useLoadScript, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+
+const libraries = ["places"];
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh'
+}
+const center = {
+    lat: -37.813629,
+    lng: 144.963058
+}
 
 export default function Map() {
-    const [viewport, setViewport] = useState({
-        longitude: 144.962316,
-        latitude: -37.812282,
-        width: "100vh",
-        height: "70vh",
-        zoom: 10
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
     });
+
+    const mapRef = useRef();
+    const onMapLoad = useCallback((map) => {
+        mapRef.current = map;
+    });
+
+    if (loadError) return "Error loading map";
+    if (!isLoaded) return "Loading Map..";
     
     return (
         <div className="flex-centered" style={{margin: "2em 0 8px 0"}} >
-            <ReactMapGL {...viewport} 
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-            onViewportChange={viewport => {
-                setViewport(viewport);
-            }}
-            mapStyle="mapbox://styles/lukey88/ckg329w4w06p319o0nqrlg6sc"
-            >
-                <Source type="geojson" data={mapData}>
-                    <Layer {...dataLayer}/>
-                </Source>
-            </ReactMapGL>
+           <GoogleMap 
+           mapContainerStyle={mapContainerStyle} 
+           zoom={8} 
+           center={center}
+           onClick={(e) => {
+               console.log(e);
+           }}
+           onLoad={onMapLoad}
+           >
+
+           </GoogleMap>
         </div>
     )
 }
