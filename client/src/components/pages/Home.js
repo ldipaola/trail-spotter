9f9001a07e7d428a56e8a2fe27534ee6c4463ce9
Lukey <ldipaola66@gmail.com>
2020-10-18 20:23:55 +1100

    merge blog and home component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,29 +1,13 @@
 import React, { useContext } from "react";
 import UserContext from "../../utils/UserContext";
+import Blog from "../blog/Blog";
 
 export default function Home(props) {
   const { user } = useContext(UserContext);
 
   return (
-    <div className="container grid-lg" style={{marginTop: "2em"}}>
-      <h1>Welcome</h1>
-
-      {user ? <pre>{user.email}</pre> : <pre>Not Logged in</pre>}
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
-    </div>
+    <>
+    <Blog />
+    </>
   );
 }
