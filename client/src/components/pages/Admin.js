e716a4601536e0171cca3e166ce78544d5724428
Lukey <ldipaola66@gmail.com>
2020-10-19 07:45:29 +1100

    add buttons

--- client/src/components/pages/Admin.js
+++ client/src/components/pages/Admin.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import PostForm from "../post-form/PostForm"
+import React, {useState} from "react";
+import PostForm from "../admin-tools/PostForm";
+
+
+
 
 export default function Admin() {
-    return (
-        <div className="container grid-lg">
-            <PostForm />
+    const [component, setComponent] = useState(PostForm);
+
+  return (
+    <div className="container grid-lg">
+        <div className="columns">
+            <div className="column col-4">
+            <ul style={{listStyle: "none"}}>
+          <li><button style={{width: "180px"}}  className="btn">New Post</button></li>
+          <li><button style={{width: "180px"}} className="btn" >Edit and Delete posts</button></li>
+          <li><button style={{width: "180px"}} className="btn">Manage Markers</button></li>
+          <li><button style={{width: "180px"}} className="btn">Users</button></li>
+      </ul>
+            </div>
+            <div className="column col-8">
+              <PostForm />
+      </div>
         </div>
-    )
+    </div>
+  );
 }
